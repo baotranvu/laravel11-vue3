0c641713df9bb5e3e916e7b5a6f271c5078f82e2
baotran <tranvubao195@gmail.com>
2024-12-20 23:15:13 +0700

    add Task api

--- src/todo-list/routes/api.php
+++ src/todo-list/routes/api.php
@@ -5,14 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::group(['prefix' => 'v1'], base_path('routes/Api/V1/Task.php'));
     Route::group(['prefix' => 'v2'], base_path('routes/Api/V2/Task.php'));
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
+Route::group(['prefix' => 'v1'], base_path('routes/Api/V1/Task.php'));
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
 
 Route::group(['prefix' => 'auth'], base_path('routes/Api/Auth/Auth.php'));
 
