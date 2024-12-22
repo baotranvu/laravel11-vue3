8f3dd38dcf09c75c59338211edc78efe7daad073
baotran <tranvubao195@gmail.com>
2024-12-22 19:51:51 +0700

    apiv1frontend: add complete task api frontend

--- src/vue3-app/src/services/TaskService.ts
+++ src/vue3-app/src/services/TaskService.ts
@@ -1,10 +1,13 @@
 // src/services/TaskService.ts
 import ResourceService from "./ResourceService";
 import { Task, TaskPayload } from "../types/Task";
-
+import api from "../http/api";
 class TaskService extends ResourceService<Task, TaskPayload> {
   constructor() {
     super("tasks");
+  }
+  async changeTaskStatus(id: number) {
+    return api.patch(`tasks/${id}/complete`);
   }
 }
 
