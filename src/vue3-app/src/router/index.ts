de403296d52d15c57bddcdea6c1fe879ba696dc5
baotran <tranvubao195@gmail.com>
2025-01-01 17:18:25 +0700

    config api

--- src/vue3-app/src/router/index.ts
+++ src/vue3-app/src/router/index.ts
@@ -1,16 +1,1 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ErrorPage from '@/components/error/ErrorPage.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: ErrorPage
-    }
-    // ... your other routes
-  ]
-})
-
-export default router
--- src/vue3-app/vite.config.ts
+++ src/vue3-app/vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }), // Enabled Vuetify auto import
+  ],
   server: {
-    host: 'todo.test', // This is the hostname that will be used in the browser
+    host: 'todo.test',
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // This allows you to use `@` as an alias for `src/`
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
