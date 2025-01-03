09aa01d2ff82c1c8cdda20217cdd7034bb40d4bf
baotran <tranvubao195@gmail.com>
2025-01-03 23:34:03 +0700

    add verify mail after register

--- src/todo-list/routes/api.php
+++ src/todo-list/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::group(['prefix' => 'v2'], base_path('routes/Api/V2/Task.php'));
 });
 
