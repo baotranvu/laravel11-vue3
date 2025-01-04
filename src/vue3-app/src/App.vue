6a8e358f5833c643e584d883c15da5dbf9df6283
baotran <tranvubao195@gmail.com>
2025-01-05 01:22:39 +0700

    Refactor authentication and task management
    - Updated App.vue to conditionally render Navbar based on route.
    - Improved AuthService to handle token management more efficiently.
    - Enhanced ResourceService to set authorization headers based on stored tokens.
    - Refactored useTaskCard composable to include task completion and deletion functionality.
    - Adjusted LoginPage to reset authentication state on mount.
    - Updated getters in auth store to retrieve token from session storage if not available in state.

--- src/vue3-app/src/App.vue
+++ src/vue3-app/src/App.vue
@@ -1,12 +1,24 @@
 
 <script setup>
 import AppFooter from "@/components/AppFooter.vue";
-
 </script>
 <template>
-    <router-view></router-view>
+    <Navbar v-if="!['login', 'register'].includes($route.name)" />
+    <div class="main-container">
+        <router-view></router-view>
+    </div>
     <AppFooter />
 </template>
+<style scoped>
+    .main-container {
+        width: 100%;
+        min-height: 100vh;
+        background-image: url('./assets/images/bg.jpg');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
+
 
 
 
