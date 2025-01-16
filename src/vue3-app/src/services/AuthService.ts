4142f15ed4962f1eaf23bd9b840b2933bed58e8f
baotran <tranvubao195@gmail.com>
2025-01-16 18:14:48 +0700

    [Auth] remove csrf token call

--- src/vue3-app/src/services/AuthService.ts
+++ src/vue3-app/src/services/AuthService.ts
@@ -1,11 +1,9 @@
 import { User, LoginCredentials, RegisterData } from '@/types/Auth';
 import { api } from '@/http/api';
 import { useAuthStore } from '@/stores/auth';
-import axios from 'axios';
 export class AuthService {
     private static instance: AuthService;
     private readonly apiUrl = 'api/auth';
-    private readonly baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://api.todo-list.com:8080';
     private isInterceptorInitialized = false;
     private constructor() {}
     private getInterceptorsInstance() {
@@ -29,7 +27,6 @@ export class AuthService {
     }
 
     async login(credentials: LoginCredentials): Promise<User> {
-        await axios.get(`${this.baseUrl}/sanctum/csrf-cookie`);
         const response = await api.post(`${this.apiUrl}/login`, credentials);
         return response.data;
     }
